cee63d3d4c034d2696d14ec4ef532aaaa2d4d656
Eden <edenmersha2721@gmail.com>
2026-05-31 15:25:50 +0300

    Update inventory response and query handler

--- inventory/src/main/java/com/medicinelocator/inventory/application/query/handler/GetMedicineByIdHandler.java
+++ inventory/src/main/java/com/medicinelocator/inventory/application/query/handler/GetMedicineByIdHandler.java
@@ -1,4 +1,29 @@
 package com.medicinelocator.inventory.application.query.handler;
 
+import com.medicinelocator.inventory.application.dto.response.MedicineResponse;
+import com.medicinelocator.inventory.application.mapper.MedicineMapper;
+import com.medicinelocator.inventory.application.query.GetMedicineByIdQuery;
+import com.medicinelocator.inventory.application.service.MedicineService;
+import com.medicinelocator.inventory.domain.exception.MedicineNotFoundException;
+import com.medicinelocator.inventory.domain.model.Medicine;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
 public class GetMedicineByIdHandler {
+
+    private final MedicineService medicineService;
+    private final MedicineMapper medicineMapper;
+
+    public GetMedicineByIdHandler(MedicineService medicineService, MedicineMapper medicineMapper) {
+        this.medicineService = medicineService;
+        this.medicineMapper = medicineMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public MedicineResponse handle(GetMedicineByIdQuery query) {
+        Medicine medicine = medicineService.findById(query.getMedicineId())
+                .orElseThrow(() -> new MedicineNotFoundException(query.getMedicineId()));
+        return medicineMapper.toMedicineResponse(medicine);
+    }
 }
